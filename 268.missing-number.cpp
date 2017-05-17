c0e7ca677cbb327b62580db393ef424f84f6bd72
Zixin Liu <liuzixin@umich.edu>
2017-05-17 13:38:37 -0400

    268

---
+++ 268.missing-number.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+    // initial thought, try to sort first and compare with targets, 
+    // the complexity of sort function is algorithm library is O(nlogn),
+   	// this doesn't meet complexity requirement
+
+
+    // 	// sort the nums in increasing order
+    // 	sort(nums.begin(), nums.end());
+
+    // 	// compare the elements to 0 to size - 1
+    // 	for(int i = 0; i < int(nums.size()); ++i) {
+    // 		if(nums[i] != i) {
+    // 			// if the element is different, i is missing
+    // 			return i;
+    // 		}
+    // 	}
+    // 	// if all elements are desired, the missing element is n, 
+    // 	// which is the size of nums
+    // 	return int(nums.size());
+    // }
+
+
+
+    // faster version----- geometric sequence
+    	int sum = int(nums.size())* (int(nums.size()) + 1 )/ 2;
+    	int nums_sum = 0;
+    	for (int i = 0; i < int(nums.size()); ++i) {
+    		nums_sum += nums[i];
+    	}
+    	if(sum - nums_sum == 0) {
+    		return 0;
+    	} else {
+    		return sum - nums_sum;
+    	}
+    }
+    // OTHER METHOD : BIT MANIPULATION ---SAVE FOR LATER
+
+};
