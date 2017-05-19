4165cb751e69f5ef70b42a31e1c3c61c89118b9d
Zixin Liu <liuzixin@umich.edu>
2017-05-19 19:57:40 -0400

    442 without extra memory and O(n)

--- 442.find-all-duplicates-in-an-array.cpp
+++ 442.find-all-duplicates-in-an-array.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
-        
+        vector<int> result;
+        // negative appear once, positive never appear, 0 appear twice
+        for(int i = 0;  i < int(nums.size()); ++i) {
+            if(nums[abs(nums[i]) - 1] < 0) {
+                result.push_back(abs(nums[i]));
+            }
+            if(nums[abs(nums[i]) - 1] > 0) {
+                nums[abs(nums[i]) - 1] = - nums[abs(nums[i]) - 1];
+            }
+        }
+         return result;
     }
 };
+
