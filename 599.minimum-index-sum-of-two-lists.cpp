c2335a177f267d6e6487d6624176f470fa9758f1
Zixin Liu <liuzixin@umich.edu>
2017-06-28 12:47:11 -0400

    599 min idx sum of 2 lists using hash table

--- 599.minimum-index-sum-of-two-lists.cpp
+++ 599.minimum-index-sum-of-two-lists.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
-        
+        unordered_map<string, int> hash_1;
+        unordered_map<string, int> hash_2;
+        for(int i = 0; i < int(list1.size()); ++i) {
+        	hash_1[list1[i]] = i;
+        }
+        for(int  i = 0; i < int(list2.size()); ++i) {
+        	hash_2[list2[i]] = i;
+        }
+
+        map<int, vector<string>> map;
+        for(auto it = hash_1.begin(); it != hash_1.end(); ++it) {
+        	if(hash_2.find(it -> first) != hash_2.end()) {
+        		map[it -> second + hash_2[it -> first]].push_back(it -> first);
+        	}
+        }
+        return map.begin() -> second;
     }
 };
