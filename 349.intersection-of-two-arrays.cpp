cb0b27feed3c56d41eaae5b9827f18a0f98529a8
Zixin Liu <liuzixin@umich.edu>
2017-06-28 13:11:50 -0400

    intersection of 2 array using unordered_map

--- 349.intersection-of-two-arrays.cpp
+++ 349.intersection-of-two-arrays.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        
+        vector<int> result;
+        unordered_set<int> set_1;
+        for(int i = 0; i < int(nums1.size()); ++i) {
+        	set_1.insert(nums1[i]);
+        }
+  
+        for(auto it = set_1.begin(); it != set_1.end(); ++it) {
+        	if(set_.find(*it) != set_2.end()) {
+        		result.push_back(*it);
+        	}
+        }
+        return result;
     }
 };
