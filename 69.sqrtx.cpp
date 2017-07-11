319ba2b1aa44c2b1cbe5e2b7f239784131c2c497
Zixin Liu <liuzixin@umich.edu>
2017-07-11 11:47:23 -0400

    corner case, x = 0, left = 1, overflow, return answer = left - 1, *****

--- 69.sqrtx.cpp
+++ 69.sqrtx.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
     int mySqrt(int x) {
-    	// newton iterative method
-    	if(x == 0) return 0;
-    	double temp = x;
-    	// handle overflow
-    	while(fabs(temp* temp - x) > 0.0001) {
-    		temp = (temp + x /temp) / 2;
-    	} 
-    	return temp;
+    	// // newton iterative method
+    	// if(x == 0) return 0;
+    	// double temp = x;
+    	// // handle overflow
+    	// while(fabs(temp * temp - x) > 0.0001) {
+    	// 	temp = (temp + x /temp) / 2;
+    	// } 
+    	// return temp;
 
 
     	// // binary search method
-     //    if (0 == x) return 0;
-     //    int left = 1, right = x, ans;
-     //    while (left <= right) {
-     //        int mid = left + (right - left) / 2;
-     //        if (mid <= x / mid) {
-     //            left = mid + 1;
-     //            ans = mid;
-     //        } else {
-     //            right = mid - 1;
-     //        }
-     //    }
-     //    return ans;
-    
+        if (0 == x) return 0;
+        
+        // if we keep left = 0, when we have x = 0, we divide by 0, runtime error
+        int left = 1, right = x;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            // since sqrt of 5 and other, never equal, merge < = condiition
+            if (mid <= x / mid) {
+                left = mid + 1;
+                //ans = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        // left always keep the biggest integer whose square is closest to x
+        // the reason is minus 1 is that, the last step of while loop is add 1 to left
+        // so that left could greater than right, therefore a step backwards is th answer
+        return left - 1;
         
     }
 };
