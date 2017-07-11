cd7e308d12fd2daccaf4166f78caa58af7af9686
Zixin Liu <liuzixin@umich.edu>
2017-07-11 15:04:44 -0400

    lca of binary tree,****

--- 236.lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236.lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,19 +9,26 @@
  */
 class Solution {
 public:
-
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-    	if (left == null)
-    	{
+    	
+    	// can't just comapre the p, q value with root, they have to be the same node, since there are dupliacte values
+    	if(root == nullptr || root == p  || root == q ) {
+    		return root;
+    	}
+    	// since the root is not the LCA of p, q, go to its left and right subtree
+    	TreeNode * left = lowestCommonAncestor(root -> left, p, q);
+    	TreeNode * right = lowestCommonAncestor(root -> right, p, q);
+    	
+    	// if find p q in current tree, but root is not p, q return root;
+    	if(left && right) return root;
+
+    	// if could not find lca in left, then both q,p in the right, return right, vice versa.
+    	if(left == nullptr) {
     		return right;
     	} else {
-    		if (right == null)
-    		{
-    			return left;
-    		} else {
-    			return root;
-    		}
+    		return left;
     	}
-        return left == null ? right : right == null ? left : root;
     }
+
+	
 };
