15856ef91524578d6c519f9d3c2396c4289497d3
Zixin Liu <liuzixin@umich.edu>
2017-07-12 14:35:43 -0400

    using recursion, always cut hlf until n is 0 and base case is 1, comment updated

--- 50.powx-n.cpp
+++ 50.powx-n.cpp
@@ -2,6 +2,8 @@ class Solution {
 public: 
 	double powHelp(double x, int n) {
 		// recursion, n always back to 0
+		// base case can't be n == 1 since when --a, will overflow, 
+		//n will be negative, it will enver end to -1
 		if(n == 0) return 1;
 		double half = powHelp(x, n / 2);
 		if(n % 2 == 0) return half * half;
