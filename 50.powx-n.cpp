df679bdf16ba89c20f0775a5451d91d2da4fae7d
Zixin Liu <liuzixin@umich.edu>
2017-07-12 14:05:24 -0400

    using recursion, always cut hlf until n is 0 and base case is 1

--- 50.powx-n.cpp
+++ 50.powx-n.cpp
@@ -1,6 +1,18 @@
 class Solution {
-public:
+public: 
+	double powHelp(double x, int n) {
+		// recursion, n always back to 0
+		if(n == 0) return 1;
+		double half = powHelp(x, n / 2);
+		if(n % 2 == 0) return half * half;
+		return x * half * half;
+	}
     double myPow(double x, int n) {
+    	// handle positive and negative
+    	if(n < 0) {
+    		return 1 / powHelp(x, -n);
+    	}
+ 		return powHelp(x, n); 
         
     }
 };
