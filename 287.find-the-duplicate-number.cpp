20d513a7dc47fd7bab5af6376cc5ffcd03ce74d2
Zixin Liu <liuzixin@umich.edu>
2017-07-13 19:08:26 -0400

    refer 142, it must have a cycle, jump the the next index given by the current array val

--- 287.find-the-duplicate-number.cpp
+++ 287.find-the-duplicate-number.cpp
@@ -1,16 +1,38 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-    	int res = -1;
-    	for(int i = 0; i < int(nums.size()); ++i){
-    		if(nums[abs(nums[i])] > 0) {
-    			nums[abs(nums[i])] *= -1;
-    		} else {
-    			res = abs(nums[i]);
-    			return res;
-    		}
-    	}
-    	return res;
-        
+    	
+    	// modify the array 
+    	// int res = -1;
+    	// for(int i = 0; i < int(nums.size()); ++i){
+    	// 	if(nums[abs(nums[i])] > 0) {
+    	// 		nums[abs(nums[i])] *= -1;
+    	// 	} else {
+    	// 		res = abs(nums[i]);
+    	// 		return res;
+    	// 	}
+    	// }
+    	// return res;
+
+    	int slow = nums[0];
+    	int fast = nums[0];
+    	int curhead = nums[0];
+    	int curinter;
+
+	    while(true){
+	    	slow = nums[slow];
+	    	fast = nums[fast];
+	    	fast = nums[fast];
+	    	if(slow == fast) {
+	    		curinter = slow;
+	    		break;
+	    	}
+	    }
+	    if(curinter == curhead) return curhead;
+	    while(curhead != curinter) {
+	    	curinter = nums[curinter];
+	    	curhead = nums[curhead];
+	    }
+	    return curhead;   
     }
 };
