ab184d4c6e98c88dd1b0256a0f89f5d82f02c334
Zixin Liu <liuzixin@umich.edu>
2017-07-13 17:39:40 -0400

    find start of cycle, based on 141 to determine the existence of cycle, make 2 ptrs start from intersection and head with the 1 step forwards, the position they meet is the start

--- 142.linked-list-cycle-ii.cpp
+++ 142.linked-list-cycle-ii.cpp
@@ -8,7 +8,42 @@
  */
 class Solution {
 public:
+	void detectCycleHelp(ListNode *head, ListNode * &meet) {
+        ListNode * l1 = head;
+        ListNode * l2 = head;
+        if(head == nullptr) return;
+        do{
+        	l1 = l1 -> next;
+        	l2 = l2 -> next;
+        	if(l2 !=  nullptr) {
+        		l2 = l2 -> next;
+        		if(l1 == l2) {
+        			meet = l1;
+        			return;
+        		}
+        	}
+        } while(l1 && l2);
+    }
+
     ListNode *detectCycle(ListNode *head) {
-        
+    	ListNode * meet = nullptr;
+    	// if they don't meet, means they don't have cycle, return nullptr
+    	detectCycleHelp(head, meet);
+    	if(meet == nullptr) return nullptr;
+
+    	// if they meet, one ptr =  head, the other one =  meet;
+    	// move in the same speed, they must meet
+    	ListNode * t1 = head;
+    	ListNode * t2 = meet;
+    	while(t1 != t2) {
+    		t1 = t1 -> next;
+    		t2 = t2 -> next;
+    	}
+    	return t1;    
     }
 };
+
+
+//http://bangbingsyb.blogspot.com/2014/11/leetcode-linked-list-cycle-i-ii.html
+
+
